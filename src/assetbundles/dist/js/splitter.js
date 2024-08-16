5f104c826720ee8ab94d8638cd7a68d2f96c60a2
Ivan Juarez <ivan@madebyraygun.com>
2024-08-16 11:54:36 -0600

    feat: splitter in preview iframe

--- src/assetbundles/dist/js/splitter.js
+++ src/assetbundles/dist/js/splitter.js
@@ -1,10 +1,13 @@
+import { LibraryComponent } from './base/library-component.js';
 import 'https://cdnjs.cloudflare.com/ajax/libs/split.js/1.6.0/split.min.js';
 /**
  * Uses https://github.com/nathancahill/split/tree/master/packages/splitjs
  */
 
-export class Splitter {
+export class Splitter extends LibraryComponent {
   constructor() {
+    super();
+
     Split([
       '#split-sidebar',
       '#split-content',
@@ -23,6 +26,19 @@ export class Splitter {
       direction: 'vertical',
       sizes: [60, 40],
       gutterSize: 8,
+    })
+
+    Split([
+      '#preview-iframe',
+      '#split-preview-resize'
+    ], {
+      direction: 'horizontal',
+      minSize: 0,
+      sizes: [100, 0],
+      gutterSize: 8,
+      onDrag: function () {
+        console.log('dragging')
+      },
     })
   }
 }
